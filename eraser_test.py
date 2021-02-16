2cc0ab813170b83aea9cbe821471b24c414718e4
a-alex-l <lesnik-a-a@yandex.ru>
2021-02-16 13:32:00 +0300

    Added shadow eraser and tests. Cleaner using shadow eraser before ridding of grid.

---
+++ eraser_test.py
@@ -0,0 +1,40 @@
+import unittest
+import os
+from PIL import Image
+
+inaccuracy = 0.5  # jpg loss = 0.02 = 2% = 5/256
+
+
+def compare_images(infile, outfile):
+    while not os.path.exists(infile):
+        pass
+    difference = 0
+    image_ans = Image.open(infile)
+    image_out = Image.open(outfile)
+    for i in range(image_ans.size[0]):
+        for j in range(image_ans.size[1]):
+            difference += abs(image_ans.getpixel((i, j))[0] - image_out.getpixel((i, j))[0])
+            difference += abs(image_ans.getpixel((i, j))[1] - image_out.getpixel((i, j))[1])
+            difference += abs(image_ans.getpixel((i, j))[2] - image_out.getpixel((i, j))[2])
+    image_ans.close()
+    image_out.close()
+    return difference / 3 / 255 / image_ans.size[0] / image_ans.size[1]
+
+
+class MyTestCase(unittest.TestCase):
+    def test_all(self):
+        argv = ""
+        for i in range(1, 29):
+            argv += " assets/eraser/test_input_" + str(i) + ".png"
+        os.system('python shadow_eraser.py' + argv)
+        for i in range(1, 29):
+            score = compare_images("assets/eraser/test_input_" + str(i) + "_ans.png",
+                                   "assets/eraser/test_output_" + str(i) + ".png")
+            print(i, "=>", score, end='    ')
+            if i % 5 == 0:
+                print(flush=True)
+            self.assertTrue(score <= inaccuracy)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ shadow_eraser.py
@@ -0,0 +1,86 @@
+import sys
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import cv2
+
+
+def sign(x_for_sign):
+    return math.copysign(1, x_for_sign)
+
+
+def get_approximate_color(i, j, gray_image, mask):
+    max_side = max(0, min(i, j, mask.shape[0] - i - 1, mask.shape[1] - j - 1))
+    side = 20
+    for step in range(int(math.log2(max_side + 2))):
+        i_new = i + random.randint(-side, side)
+        j_new = j + random.randint(-side, side)
+        if 0 <= i_new < mask.shape[0] and 0 <= j_new < mask.shape[1] and mask[i_new, j_new] == 0:
+            return gray_image[i_new, j_new]
+        if side * 2 < max_side:
+            side = int(side * 1.5)
+        else:
+            side = max_side
+    return 77
+
+
+def get_f_color(shadow_color, current_color, global_average):
+    x = float(shadow_color) - global_average
+    y = float(current_color) - global_average
+    return (x + y) / (((x - y)/50)**4 + 2)
+
+
+def get_shadow_distribution(gray_image, mask):
+    ans_image = gray_image.copy()
+    for i in range(ans_image.shape[0]):
+        for j in range(ans_image.shape[1]):
+            if mask[i, j] != 0:
+                ans_image[i, j] = get_approximate_color(i, j, ans_image, mask)
+    ans_image = cv2.blur(ans_image, (101, 101))
+    global_average = int(np.average(ans_image))
+    for i in range(ans_image.shape[0]):
+        for j in range(ans_image.shape[1]):
+            ans_image[i, j] = get_f_color(ans_image[i, j], gray_image[i, j], global_average)
+    # cv2.imshow("gray" + in_file, ans_image)
+    return ans_image
+
+
+def get_mask_image(mask) -> np.ndarray:
+    mask = cv2.adaptiveThreshold(mask,
+                                 maxValue=255,
+                                 adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                 thresholdType=cv2.THRESH_BINARY_INV,
+                                 blockSize=101,  # Parameter!
+                                 C=5)  # Parameter!
+    return mask
+
+
+def save_ans(ans_image, in_file_name):
+    dot_index = in_file_name.find('.')
+    ans_file_name = in_file_name[:dot_index] + "_ans.png"
+    try:
+        cv2.imwrite(ans_file_name, ans_image)
+    except OSError:
+        raise Warning("Warning: Skipped file " + in_file)
+        pass
+
+
+def rid_of_shadow(image, in_file_name):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    mask = get_mask_image(gray_image)
+    gray_image = get_shadow_distribution(gray_image, mask)
+    ans_image = image - cv2.cvtColor(gray_image, cv2.COLOR_GRAY2RGB)
+    # cv2.imshow("ans" + in_file, ans_image)
+    save_ans(ans_image, in_file_name)
+
+
+if __name__ == '__main__':
+    for in_file in sys.argv[1:]:
+        try:
+            rid_of_shadow(cv2.imread(in_file), in_file)
+        except OSError:
+            raise Warning("Warning: Skipped file " + in_file)
+            pass
+
+# cv2.waitKey(0)
